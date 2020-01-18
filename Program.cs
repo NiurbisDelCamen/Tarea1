346f5e812c5433c60ba4ffa70a8486160f87af59
Niurbis Del Carmen Reyes Jaquez <niurbis_reyes@ucne.edu.do>
2020-01-18 17:19:42 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tarea1ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 1");
+            Console.WriteLine("Niurbis");
+            Console.WriteLine("Ejercicio 2");
+            Console.WriteLine("Estos");
+            Console.WriteLine("Son");
+            Console.WriteLine("mensajes");
+            Console.WriteLine("de prueba.");
+
+
+        }
+    }
+}
